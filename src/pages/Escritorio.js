4266e7867ae6a5af87d8367e06a0e035c53848ce
Helis Montes <helismontes@gmail.com>
2021-05-14 22:18:24 -0500

    Asignar un ticket para trabajar

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Col, Divider, Row, Typography } from 'antd';
 import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
 import { useHideMenu } from '../hooks/useHideMenu';
 import { getUserStorage } from '../helpers/getUserStorage';
 import { Redirect, useHistory } from 'react-router';
+import { SocketContext } from '../context/SocketContext';
 
 
 const {Text, Title} = Typography
@@ -11,13 +12,18 @@ const {Text, Title} = Typography
 export const Escritorio = () => {
   const history = useHistory()
   useHideMenu(false);
-  const [user] = useState(getUserStorage())
+  const {socket} = useContext(SocketContext);
+  const [user] = useState(getUserStorage());
+  const [ticket, setTicket] = useState({});
+
   const salir = () => {
     localStorage.clear();
     history.replace('/ingresar');
   }
   const siguiente = () => {
-    console.log('Next.......')
+    socket.emit('next-ticket', user, (ticket) => {
+      setTicket(ticket);
+    });
   }
   if (!user.agente || !user.escritorio) return <Redirect to="/ingresar"/>
   return (
@@ -39,15 +45,30 @@ export const Escritorio = () => {
         </Col>
       </Row>
       <Divider />
-      <Row>
-        <Col spam={20}>
-          <Text>Esta atendiendo el ticket: </Text>
-          <Text 
-            style={{fontSize: 30}}
-            type="danger"
-          >55</Text>
-        </Col>
-      </Row>
+      {
+        ticket?.id 
+        ? (
+          <Row>
+            <Col spam={20}>
+              <Text>Esta atendiendo el ticket: </Text>
+              <Text 
+                style={{fontSize: 30}}
+                type="danger"
+              >{ticket.numero}</Text>
+            </Col>
+          </Row>
+          )
+        : (
+          <Row>
+            <Col spam={20}>
+              <Text
+                type="success"
+                style={{fontSize: 20}}
+              >No hay tickets en cola</Text>
+            </Col>
+          </Row>
+          )
+      }
       <Row>
         <Col offset={20} spam={6} align="right">
           <Button 
