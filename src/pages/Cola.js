7ee6596ae69d132212f59c187a9e29fd57afeace
Helis Montes <helismontes@gmail.com>
2021-05-14 23:07:35 -0500

    Actualizar la cola de tickets en tiempo real

--- src/pages/Cola.js
+++ src/pages/Cola.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { Col, Row, Typography, List, Card, Tag, Divider } from 'antd';
-import data from '../data';
 import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
-const { Text, Title } = Typography
+const { Text, Title } = Typography;
 
 export const Cola = () => {
   useHideMenu(true);
+  const {socket} = useContext(SocketContext);
+  const [ticketList, setTicketList] = useState([]);
+  useEffect(() => {
+    socket.on('tickets-asignados', (tickets) => {
+      setTicketList(tickets);
+    });
+    return () => {
+      socket.off('tickets-asignados');
+    }
+  }, [socket]);
   return (
     <>
       <Title level={1}>Atendiendo al cliente</Title>
       <Row>
         <Col span={12}>
           <List
-            dataSource={data.slice(0,3)}
+            dataSource={ticketList.slice(0,3)}
             renderItem={ item => (
                <List.Item>
                 <Card
@@ -23,33 +33,37 @@ export const Cola = () => {
                     <Tag color="magenta">Escritorio{item.escritorio}</Tag>,
                   ]}
                 >
-                  <Title>No. {item.ticketNo}</Title>
+                  <Title>No. {item.numero}</Title>
                 </Card>
                </List.Item>
             )}
           />
         </Col>
-        <Col span={12}>
-          <Divider>Historial</Divider>
-          <List
-            dataSource={data.slice(3)}
-            renderItem={item =>(
-              <List.Item>
-                <List.Item.Meta
-                  title={`Ticket No. ${item.ticketNo}`}
-                  description={
-                    <>
-                      <Text type="secondary">En el escritorio: </Text>
-                      <Tag color="magenta">{item.escritorio}</Tag>
-                      <Text type="secondary">Agente: </Text>
-                      <Tag color="volcano">{item.agente}</Tag>
-                    </>
-                  }
-                />
-              </List.Item>
-            )}
-          />
-        </Col>
+        {
+          (ticketList.length > 3) && (
+             <Col span={12}>
+              <Divider>Historial</Divider>
+              <List
+                dataSource={ticketList.slice(3)}
+                renderItem={item =>(
+                  <List.Item>
+                    <List.Item.Meta
+                      title={`Ticket No. ${item.numero}`}
+                      description={
+                        <>
+                          <Text type="secondary">En el escritorio: </Text>
+                          <Tag color="magenta">{item.escritorio}</Tag>
+                          <Text type="secondary">Agente: </Text>
+                          <Tag color="volcano">{item.agente}</Tag>
+                        </>
+                      }
+                    />
+                  </List.Item>
+                )}
+              />
+            </Col>
+          )
+        }
       </Row>
     </>
   )
