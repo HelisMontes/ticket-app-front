689da15ab42f634e90e099e616861727c32bc0ad
Helis Montes <helismontes@gmail.com>
2021-05-14 20:14:51 -0500

    Obtener un nuevo ticket

--- src/pages/CrearTicket.js
+++ src/pages/CrearTicket.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Col, Row, Typography } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons'
 import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
 const { Text, Title } = Typography
 
 export const CrearTicket = () => {
   useHideMenu(true);
+  
+  const { socket } = useContext(SocketContext); 
+  const [ ticket, setTicket] = useState({});
   const newTicket = () => {
-    console.log('New');
+    socket.emit('solicitar-ticket', null, (ticket) => {
+      setTicket(ticket);
+    });
   }
   return (
     <>
@@ -27,17 +33,21 @@ export const CrearTicket = () => {
           </Button>
         </Col>
       </Row>
-      <Row style={{ marginTop: 100 }}>
-        <Col span={14} offset={6} align="center">
-          <Text level={2}>
-            Su número
-          </Text>
-          <br/>
-          <Text type="success" style={{fontSize: 55}}>
-            55
-          </Text>
-        </Col>
-      </Row>
+      {
+        ticket?.numero && (
+          <Row style={{ marginTop: 100 }}>
+            <Col span={14} offset={6} align="center">
+              <Text level={2}>
+                Su número
+              </Text>
+              <br/>
+              <Text type="success" style={{fontSize: 55}}>
+                {ticket.numero}
+              </Text>
+            </Col>
+          </Row>
+        )
+      }
     </>
   )
 }
